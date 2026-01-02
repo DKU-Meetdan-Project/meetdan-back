7e5d44350d0cd389442435c88f3680a16f2ab6fb
고경수 <teddy6848@gmail.com>
2026-01-02 17:20:35 +0900

    init backend project

---
+++ meetdan/src/main/java/org/example/meetdan/MeetdanApplication.java
@@ -0,0 +1,13 @@
+package org.example.meetdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetdanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetdanApplication.class, args);
+    }
+
+}
---
+++ meetdan/src/test/java/org/example/meetdan/MeetdanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meetdan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetdanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
